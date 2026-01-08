7b32f614f5bd3a41074e711410275ace184a1381
Brian <clockworkai@outlook.com>
2026-01-08 18:35:23 +0000

    fix: use static prefixer to prevent SSR hydration mismatch

--- source/utils/genStyles.js
+++ source/utils/genStyles.js
@@ -1,18 +1,16 @@
-import Prefixer from 'inline-style-prefixer'
+import prefixAll from 'inline-style-prefixer/static'
 
 import replacedStyleFn from './replacedStyleFn'
 import replaceColors from './replaceColors'
-import {specialCharacters} from './index'
+import { specialCharacters } from './index'
 
-const prefixer = new Prefixer();
+export default function genStyles(styleStuff, args, colors) {
+  const userResult = replacedStyleFn(styleStuff, args);
 
-export default function genStyles(styleStuff, args,colors){
-  const userResult = replacedStyleFn(styleStuff,args);
-
-  const swapedColor = replaceColors(colors,userResult);
-  for(const name in swapedColor){
+  const swapedColor = replaceColors(colors, userResult);
+  for (const name in swapedColor) {
     //if(!specialCharacters.includes(name[0]))
-    swapedColor[name] = prefixer.prefix({a:swapedColor[name]}).a;
+    swapedColor[name] = prefixAll({ a: swapedColor[name] }).a;
   }
 
   return swapedColor;
